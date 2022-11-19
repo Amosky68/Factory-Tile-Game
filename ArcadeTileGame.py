77fdb162fdec17a9302eb090a539e375c7194f64
Amosky <100569847+Amosky68@users.noreply.github.com>
2022-11-19 12:23:32 +0100

    Version 1.0.0

--- ArcadeTileGame.py
+++ ArcadeTileGame.py
@@ -24,11 +24,15 @@ class Generator():
 
 class Chunk():
 
-    def __init__(self , texturesLocation , chunk_size = 32 , position = (0,0)) :
+    def __init__(self , texturesLocation , seed = None ,chunk_size = 32 , position = (0,0)) :
         self.chunk_size = chunk_size
         self.position = position
         self.Tiles = None
         self.texturesLocations = texturesLocation
+        if seed is None :
+            self.seed = 0
+        else :
+            self.seed = seed
 
 
     def absoluteChunkCoordinates(self, Tileposition) :
@@ -36,33 +40,63 @@ class Chunk():
         return ( self.position[0] + Tileposition[0]/self.chunk_size  ,  self.position[1] + Tileposition[1]/self.chunk_size  )
 
 
-    def get_Tile(self,coordinates) :
-        seed = 15
-        noise = PerlinNoise( octaves = 1, seed = seed)
-        value = noise( self.absoluteChunkCoordinates(coordinates))
+    def get_Tile(self,coordinates,noises) :
+        noise = PerlinNoise( octaves = 1, seed = self.seed)
+        # on parcours tout les noises pour crée la carte la plus détaillé possible
+        value = 0
+        for i , noise in enumerate(noises):
+            value += noise(self.absoluteChunkCoordinates(coordinates)) * 0.5 ** i
+
+
+        # Generation rules
 
         tilecoordinates = (coordinates[0] + self.position[0] * self.chunk_size , coordinates[1] + self.position[1] * self.chunk_size)
         if value < 0 :
             return Ground(self.texturesLocations[0] , tilecoordinates , tilecoordinates[0] * 100 , tilecoordinates[1] * 100 , 5)
         elif value < 0.05 :
             return Ground(self.texturesLocations[1] , tilecoordinates , tilecoordinates[0] * 100 , tilecoordinates[1] * 100 , 5)
-        else :
+        elif value < 0.35 :
             return Ground(self.texturesLocations[2] , tilecoordinates , tilecoordinates[0] * 100 , tilecoordinates[1] * 100 , 5)
+        else :
+            return Ground(self.texturesLocations[3] , tilecoordinates , tilecoordinates[0] * 100 , tilecoordinates[1] * 100 , 5)
+
+
+
 
 
     def generate(self) :
         timing = time.perf_counter()
 
+        zoom = 0.35 
+        zoom *= self.chunk_size / 32
+        Noises = []
+        # Noises.append(PerlinNoise(octaves=zoom*0.25  ,seed=self.seed))
+        for i in range(6) :
+            Noises.append(PerlinNoise(octaves=zoom*0.5 * 2**i  ,seed=self.seed)) 
+        # Noises.append(PerlinNoise(octaves=zoom*1  ,seed=self.seed))
+        # Noises.append(PerlinNoise(octaves=zoom*2  ,seed=self.seed))
+        # Noises.append(PerlinNoise(octaves=zoom*4  ,seed=self.seed))
+        # Noises.append(PerlinNoise(octaves=zoom*8 ,seed=self.seed))
+        # Noises.append(PerlinNoise(octaves=zoom*16 ,seed=self.seed))
+
+
         Tiles =  [[(i,j) for i in range(self.chunk_size)] for j in range(self.chunk_size)] 
         # print(Tiles)
+        temp = Tiles[:]
         Tiles = numpy.array(Tiles)
         # print(Tiles)
 
+        for row in range(self.chunk_size):
+            for col in range(self.chunk_size) :
+                temp[col][row] = self.get_Tile(Tiles[col][row] , Noises)
+                
+        self.Tiles = numpy.array(temp)
 
-        self.Tiles = numpy.array([list(map( self.get_Tile , row )) for row in Tiles])
-        print(time.perf_counter()-timing)
+        # print(time.perf_counter()-timing)
 
 
+    def getPositions(self):
+        return self.position
 
 
 
@@ -96,23 +130,24 @@ class Game(arcade.Window) :
     def __init__(self , screen , title):
 
         super().__init__(screen[0], screen[1], title)
+        self.version = "1.0.0"
 
         self.ProcessQueue = mp.Queue()
+        self.generationSeed = 144
+        self.generationQueue = []
+        self.maxParallelProcess = 8
         self.setup()
 
         self.set_update_rate(1/200)
         self.keys = {"Z" : False , "Q" : False , "S" : False, "D" : False }
 
-        
-
-
+    
 
     def getDirectoryLocation(self):
         Location = __file__ 
         for i in range(len(Location)-1 , 0 , -1):
             if Location[i] in '\/' :
                 return Location[:i+1]
-
 
 
     def setup(self):
@@ -140,15 +175,12 @@ class Game(arcade.Window) :
 
         #     self.scene.add_sprite("Ground", water.sprite)
 
-        size = 5
-        for i in range(25) :
+        size = 20
+        for i in range(size*size) :
 
-            txtlist = [dirLocation +'textures/water.png' , dirLocation +'textures/sand.png' , dirLocation +'textures/grass.png' , dirLocation +'textures/stone.png'] 
+            self.generationQueue.append((i%size - size//2 , i//size - size//2))
+
             
-            generator = Generator(Game.generate_single_chunk , (Game,(i%size , i//size) , 32 , txtlist , self.ProcessQueue))
-            # self.generate_single_chunk((i%size , i//size) , 128 , txtlist)
-            generator.run()
-
             # [[ self.scene.add_sprite("Ground", tile.sprite) for tile in row] for row in chunk.Tiles]
 
 
@@ -174,20 +206,35 @@ class Game(arcade.Window) :
         self.center_camera_to_player( self.player )
 
         if self.keys["Z"] :
-            self.player.center_y += 1000 * deltaTime
+            self.player.center_y += 4000 * deltaTime
         if self.keys["S"] :
-            self.player.center_y -= 1000 * deltaTime
+            self.player.center_y -= 4000 * deltaTime
         if self.keys["Q"] :
-            self.player.center_x -= 1000 * deltaTime
+            self.player.center_x -= 4000 * deltaTime
         if self.keys["D"]:
-            self.player.center_x += 1000 * deltaTime
+            self.player.center_x += 4000 * deltaTime
 
+        # map Generation process
 
         if not self.ProcessQueue.empty():
-
             chunk = self.ProcessQueue.get()
+            # print(chunk)
             self.draw_single_chunk(chunk)
 
+
+
+        if len(self.generationQueue) > 0 and len(mp.active_children()) < self.maxParallelProcess:
+            # si il y a des chunks a générer et qu'on ne dépasse pas la quantité max de processus alors on génrère un nouveau chunk
+            coordinates = self.generationQueue.pop(0) 
+            dirLocation = self.getDirectoryLocation()
+            txtlist = [dirLocation +'textures/water.png' , dirLocation +'textures/sand.png' , dirLocation +'textures/grass.png' , dirLocation +'textures/stone.png'] 
+            
+            generator = Generator(Game.generate_single_chunk , (Game,coordinates , 32 , txtlist , self.generationSeed , self.ProcessQueue))
+            # self.generate_single_chunk((i%size , i//size) , 128 , txtlist)
+            generator.run()
+
+
+            
 
 
 
@@ -225,9 +272,10 @@ class Game(arcade.Window) :
     # Terrain generation 
 
 
-    def generate_single_chunk(self, Chunkposition , size , TextureList , connector):
+    def generate_single_chunk(self, Chunkposition , size , TextureList , seed ,connector ):
+        # generate a single chunk based on all the parameters
 
-        chunk = Chunk( TextureList , chunk_size=size   , position = Chunkposition )
+        chunk = Chunk( TextureList , seed = seed , chunk_size=size   , position = Chunkposition )
         chunk.generate()
     
         connector.put(chunk)
@@ -235,15 +283,18 @@ class Game(arcade.Window) :
 
 
     def draw_single_chunk(self, chunkdata) :
+        # draw a chunk using it's data
 
         t = time.perf_counter()
 
+        chunkName = chunkdata.getPositions()
+        chunkName = "Map-" + str(chunkName)
         for row in chunkdata.Tiles :
             for tile in row :
-                self.scene.add_sprite("Ground", tile.sprite)
+                self.scene.add_sprite(chunkName, tile.sprite)
 
         # [[ self.scene.add_sprite("Ground", tile.sprite) for tile in row] for row in chunkdata.Tiles]
-        print(f' i took {time.perf_counter() - t} seconds to draw this chunk')
+        # print(f'it took {time.perf_counter() - t} seconds to draw this chunk')
 
 
 
@@ -270,5 +321,11 @@ class Game(arcade.Window) :
 
 if __name__ == '__main__' : 
 
-    game = Game((800,600),"jeu")
+    game = Game((1600,1200),"jeu")
     game.run()
+
+    # clear every existing process
+
+    childprocess = mp.active_children()
+    for process in childprocess :
+        process.terminate()
